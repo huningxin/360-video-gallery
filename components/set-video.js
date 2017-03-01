869afb10041be0d1050ba080362cbaa52fe7834f
Ningxin Hu <ningxin.hu@intel.com>
2017-03-01 08:38:53 +0800

    Initial commit

---
+++ components/set-video.js
@@ -0,0 +1,23 @@
+/* global AFRAME */
+
+/**
+ * Component that listens to an event, fades out an entity, swaps the texture, and fades it
+ * back in.
+ */
+AFRAME.registerComponent('set-video', {
+  schema: {
+    on: {type: 'string'},
+    target: {type: 'selector'},
+    src: {type: 'string'},
+    dur: {type: 'number', default: 300}
+  },
+
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+
+    el.addEventListener(data.on, function () {
+        document.getElementById('video').src = data.src;
+    });
+  }
+});
---
+++ videos/README.md
@@ -0,0 +1,4 @@
+Placeholder for videos:
+congo.mp4
+honor.mp4
+shark.mp4
